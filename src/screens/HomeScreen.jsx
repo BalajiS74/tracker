4d641c7296443d77b60a72626854c64449ae2d41
BalajiS74 <balaji.sornavel7418@gmail.com>
2025-07-09 16:15:16 +0530

    updated

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -44,7 +44,7 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#f8f8f8",
+    // backgroundColor: "#f8f8f8",
   },
   header: {
     flex: 1,
